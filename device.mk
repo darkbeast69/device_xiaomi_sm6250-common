b193617cc3a7e9937b148c491ffb32c787251605
Aryan Gupta <guptaaryan189@gmail.com>
2021-03-12 19:34:42 +0000

    miatoll: nuke GoogleCameraGo and ship with Snap

--- device.mk
+++ device.mk
@@ -115,8 +115,7 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    GCamPrebuilt
-
+    Snap
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
